cf805b71a2213c4c7c23088dc1c8be7f26432581
sofijasljusar <sofija.sljusar@gmail.com>
2025-06-06 12:22:21 +0300

    Add temporary db entries with n-point

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.views import generic
 # Create your views here.
 
+import requests
+
 
 class HomeView(generic.TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_list"] = requests.get('https://api.npoint.io/ce3b8ba44b768e2a827e').json()
+        return context
