8bfb53153019a751319cfd70688e0a85795d53f8
sofijasljusar <sofija.sljusar@gmail.com>
2025-06-06 16:23:20 +0300

    Add post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="home"),
-    path('/about', views.AboutView.as_view(), name="about"),
-    path('/contact', views.ContactView.as_view(), name="contact"),
+    path("", views.HomeView.as_view(), name="home"),
+    path("/about", views.AboutView.as_view(), name="about"),
+    path("/contact", views.ContactView.as_view(), name="contact"),
+    path("/post/<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,13 @@ from django.views import generic
 
 import requests
 
-
+BLOG_POSTS = requests.get('https://api.npoint.io/ce3b8ba44b768e2a827e').json()
 class HomeView(generic.TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["post_list"] = requests.get('https://api.npoint.io/ce3b8ba44b768e2a827e').json()
+        context["post_list"] = BLOG_POSTS
         return context
 
 
@@ -20,3 +20,18 @@ class AboutView(generic.TemplateView):
 
 class ContactView(generic.TemplateView):
     template_name = "contact.html"
+
+
+class PostDetail(generic.TemplateView):
+    template_name = "post_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        slug = kwargs.get('slug')
+        post = next((p for p in BLOG_POSTS if p["slug"] == slug), None)
+        if post:
+            context["post"] = post
+        else:
+            context["post"] = {"title": "Not Found", "content": "This post does not exist."}
+
+        return context
