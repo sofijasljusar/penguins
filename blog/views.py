0dc999c6d996148ac4a15aac3be10253823d3816
sofijasljusar <sofija.sljusar@gmail.com>
2025-06-07 11:07:08 +0300

    Add Post model. Add image upload for header image. Configure Post in admin.

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ import os
 import smtplib
 
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, ListView
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from django.contrib import messages
@@ -10,6 +10,7 @@ from .forms import ContactForm
 from dotenv import load_dotenv
 # Create your views here.
 import requests
+from .models import Post
 
 load_dotenv()
 BLOG_POSTS = requests.get('https://api.npoint.io/ce3b8ba44b768e2a827e').json()
@@ -17,13 +18,14 @@ EMAIL = os.environ["EMAIL"]
 PASSWORD = os.environ["PASSWORD"]
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    queryset = Post.objects.filter(status=1)
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_list"] = BLOG_POSTS
-        return context
+    context_object_name = 'post_list'
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_list"] = BLOG_POSTS
+    #     return context
 
 
 class AboutView(TemplateView):
@@ -58,16 +60,17 @@ class ContactView(FormView):
         return super().form_valid(form)
 
 
-class PostDetail(TemplateView):
+class PostDetail(DetailView):
+    model = Post
     template_name = "post_detail.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        slug = kwargs.get('slug')
-        post = next((p for p in BLOG_POSTS if p["slug"] == slug), None)
-        if post:
-            context["post"] = post
-        else:
-            context["post"] = {"title": "Not Found", "content": "This post does not exist."}
-
-        return context
+    context_object_name = "post"
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     slug = kwargs.get('slug')
+    #     post = next((p for p in BLOG_POSTS if p["slug"] == slug), None)
+    #     if post:
+    #         context["post"] = post
+    #     else:
+    #         context["post"] = {"title": "Not Found", "content": "This post does not exist."}
+    #
+    #     return context
