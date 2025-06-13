6922f07c118df2e759062a66202eba66b5e21c89
sofijasljusar <sofija.sljusar@gmail.com>
2025-06-14 00:14:26 +0300

    Prepare for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn penguins.wsgi --bind 0.0.0.0:$PORT
--- penguins/settings.py
+++ penguins/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 import cloudinary
@@ -88,10 +89,7 @@ WSGI_APPLICATION = 'penguins.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
